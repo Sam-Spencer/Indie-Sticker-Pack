66bade58573a902ec8a804cfce9b48df4b1b3a50
Sam Spencer <samuels5@mail.usf.edu>
2020-06-21 00:46:18 -0400

    UIKit build improvements

--- Sources/UIKit/IndieStickerView.swift
+++ Sources/UIKit/IndieStickerView.swift
@@ -11,11 +11,11 @@ import UIKit
 @available(iOS 13.0, macCatalyst 13.0, *)
 @IBDesignable class IndieStickerView: UIView {
     
+    var stickerGrid: StickerGrid = StickerGrid()
     var stickerIcon: UIImageView = UIImageView()
     var titleLabel: UILabel = UILabel()
     var purchasePhysicalButton: UIButton = UIButton()
     var purchaseDigitalButton: UIButton = UIButton()
-    var subtitleLabel: UILabel = UILabel()
     var FAQbutton: UIButton = UIButton()
     var supportButton: UIButton = UIButton()
     var privacyButton: UIButton = UIButton()
@@ -31,9 +31,30 @@ import UIKit
     }
     
     func initialSetup() {
-        backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        setupGrid()
+        setupStickerIcon()
         setupTitleLabel()
-        setupSubtitleLabel()
+    }
+    
+    func setupGrid() {
+        stickerGrid.frame = CGRect(x: 0, y: 20, width: frame.width, height: frame.height - 40)
+        
+        let dimmingView = UIView(frame: stickerGrid.frame)
+        dimmingView.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        
+        addSubview(stickerGrid)
+        addSubview(dimmingView)
+        NSLayoutConstraint.activate([
+            stickerGrid.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            stickerGrid.trailingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            stickerGrid.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            stickerGrid.bottomAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor, constant: 20),
+            dimmingView.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            dimmingView.trailingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            dimmingView.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor, constant: 20),
+            dimmingView.bottomAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor, constant: 20)
+        ])
+        layoutIfNeeded()
     }
     
     func setupStickerIcon() {
@@ -52,22 +73,19 @@ import UIKit
         titleLabel.text = NSLocalizedString("Get a limited-time sticker pack from your favorite indie iOS apps.\n\nGive back to our global community.", comment: "Indie Sticker Pack: Promo Title Text")
         titleLabel.font = UIFont.systemFont(ofSize: (IndieIconPreviewGenerator.isLargerScreen ? 56 : 36), weight: .heavy)
         titleLabel.textColor = .white
+        titleLabel.numberOfLines = 0
         titleLabel.frame = CGRect(x: 20, y: 176, width: frame.size.width - 40, height: 200)
         addSubview(titleLabel)
+        NSLayoutConstraint.activate([
+            stickerIcon.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            stickerIcon.topAnchor.constraint(greaterThanOrEqualTo: stickerIcon.bottomAnchor, constant: 30)
+        ])
         layoutIfNeeded()
-    }
-    
-    func setupSubtitleLabel() {
-        subtitleLabel.text = IndieIconPreviewGenerator.isLargerScreen ? NSLocalizedString("All proceeds will be split 50/50 between the World Health Organization's COVID-19 Solidarity Response Fund and the Equal Justice Initiative for combatting racial and economic injustice.", comment: "Indie Sticker Pack: Promo Subtitle Text, iPad") : NSLocalizedString("All proceeds will be split 50/50 between the WHO's COVID-19 Solidarity Response Fund and the Equal Justice Initiative.", comment: "Indie Sticker Pack: Promo Subtitle Text, iPhone")
-        subtitleLabel.font = UIFont.systemFont(ofSize: 14, weight: .regular)
-        subtitleLabel.textColor = .white
-        titleLabel.frame = CGRect(x: 20, y: 396, width: frame.size.width - 40, height: 100)
-        addSubview(subtitleLabel)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        stickerIcon.layoutIfNeeded()
+        initialSetup()
     }
 
 }
--- Sources/UIKit/StickerGrid.swift
+++ Sources/UIKit/StickerGrid.swift
@@ -8,15 +8,141 @@
 
 import UIKit
 
+enum DiffableStickerRow {
+    case main
+}
+
+class StickerItem: UICollectionViewCell {
+    
+    var sticker: UIImageView
+    
+    override init(frame: CGRect) {
+        sticker = UIImageView(frame: frame)
+        sticker.contentMode = .scaleAspectFill
+        sticker.layer.cornerRadius = frame.width * 0.22
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        sticker = UIImageView()
+        sticker.contentMode = .scaleAspectFill
+        super.init(coder: coder)
+        sticker.layer.cornerRadius = frame.width * 0.22
+    }
+    
+    convenience init(frame: CGRect, image: UIImage) {
+        self.init(frame: frame)
+        sticker.image = image
+    }
+    
+}
+
 @available(iOS 13.0, macCatalyst 13.0, *)
 class StickerGrid: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    var rowOne = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var rowTwo = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var rowThree = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    var rowFour = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+    
+    lazy var rowOneData: DataSource = makeDataSource(for: rowOne)
+    lazy var rowTwoData: DataSource = makeDataSource(for: rowTwo)
+    lazy var rowThreeData: DataSource = makeDataSource(for: rowThree)
+    lazy var rowFourData: DataSource = makeDataSource(for: rowFour)
+    
+    let chunkedIcons = IndieIconPreviewGenerator.allIcons().chunked(into: 28)
+    
+    typealias DataSource = UICollectionViewDiffableDataSource<DiffableStickerRow, String>
+    typealias Snapshot = NSDiffableDataSourceSnapshot<DiffableStickerRow, String>
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupGrid()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupGrid()
+    }
+    
+    func setupGrid() {
+        rowOne.frame = CGRect(x: 0, y: 0, width: frame.width, height: (frame.height / 4) - 5)
+        rowOne.dataSource = rowOneData
+        rowOne.backgroundColor = .clear
+        addSubview(rowOne)
+        NSLayoutConstraint.activate([
+            rowOne.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            rowOne.trailingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            rowOne.topAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.topAnchor, constant: 0)
+        ])
+        layoutIfNeeded()
+        
+        rowTwo.frame = CGRect(x: 0, y: (frame.height / 4) + 5, width: frame.width, height: (frame.height / 4) - 5)
+        rowTwo.dataSource = rowTwoData
+        rowTwo.backgroundColor = .clear
+        addSubview(rowTwo)
+        NSLayoutConstraint.activate([
+            rowTwo.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            rowTwo.trailingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            rowTwo.topAnchor.constraint(greaterThanOrEqualTo: rowOne.topAnchor, constant: 5)
+        ])
+        layoutIfNeeded()
+        
+        rowThree.frame = CGRect(x: 0, y: ((frame.height / 4) + 5) * 2, width: frame.width, height: (frame.height / 4) - 5)
+        rowThree.dataSource = rowThreeData
+        rowThree.backgroundColor = .clear
+        addSubview(rowThree)
+        NSLayoutConstraint.activate([
+            rowThree.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            rowThree.trailingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            rowThree.topAnchor.constraint(greaterThanOrEqualTo: rowTwo.topAnchor, constant: 5)
+        ])
+        layoutIfNeeded()
+        
+        rowFour.frame = CGRect(x: 0, y: ((frame.height / 4) + 5) * 3, width: frame.width, height: (frame.height / 4) - 5)
+        rowFour.dataSource = rowFourData
+        rowFour.backgroundColor = .clear
+        addSubview(rowFour)
+        NSLayoutConstraint.activate([
+            rowFour.leadingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+            rowFour.trailingAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.trailingAnchor, constant: 0),
+            rowFour.topAnchor.constraint(greaterThanOrEqualTo: rowThree.topAnchor, constant: 5),
+            rowFour.bottomAnchor.constraint(greaterThanOrEqualTo: safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        ])
+        layoutIfNeeded()
+        
+        applySnapshot(to: rowOneData)
+        applySnapshot(to: rowTwoData)
+        applySnapshot(to: rowThreeData)
+        applySnapshot(to: rowFourData)
+    }
+    
+    func makeDataSource(for collection: UICollectionView) -> DataSource {
+        let dataSource = DataSource(collectionView: collection, cellProvider: { (collectionView, indexPath, imageName) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "stickerCell", for: indexPath) as? StickerItem
+            cell?.sticker.image = UIImage(named: imageName)
+            return cell
+        })
+        return dataSource
+    }
+    
+    func applySnapshot(to collectionData: DataSource, animatingDifferences: Bool = true) {
+        var snapshot = Snapshot()
+        snapshot.appendSections([.main])
+        if collectionData == rowOne {
+            snapshot.appendItems(chunkedIcons[0])
+        } else if collectionData == rowTwo {
+            snapshot.appendItems(chunkedIcons[1])
+        } else if collectionData == rowThree {
+            snapshot.appendItems(chunkedIcons[2])
+        } else if collectionData == rowFour {
+            snapshot.appendItems(chunkedIcons[3])
+        }
+        collectionData.apply(snapshot, animatingDifferences: animatingDifferences)
+    }
+    
+    override func layoutSubviews() {
+        setupGrid()
+    }
+    
 }
